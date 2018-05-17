2ad17b302bf9dfc25bec0b487ad698be4fb6060d
dennisat <info@anel.co>
2018-05-17 07:28:12 +0200

    test update

--- tests/unit/main.test.ts
+++ tests/unit/main.test.ts
@@ -2,12 +2,12 @@ import "jest";
 declare let window: any;
 if (typeof jasmine !== 'undefined') jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 
-import {Button} from '../../src/index';
+import {DynaInputSlider} from '../../src/index';
 
 // help: https://facebook.github.io/jest/docs/expect.html
 
 describe('Simple unit test', () => {
   it('should import the component', () => {
-		expect(Button).not.toBe(undefined);
+    expect(DynaInputSlider).not.toBe(undefined);
 	});
 });
